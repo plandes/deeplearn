fb971674a5a316ff4daf9375aee3365e9483403f
Paul Landes <landes@mailc.net>
2019-07-23 22:35:37 -0500

    fix tests

--- src/python/zensols/dltools/__init__.py
+++ src/python/zensols/dltools/__init__.py
@@ -1,5 +1,4 @@
 from zensols.dltools.plot import *
-from zensols.dltools.time import *
 from zensols.dltools.cuda_conf import *
 from zensols.dltools.linear import *
 from zensols.dltools.conv import *
