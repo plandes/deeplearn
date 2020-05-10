3cbb045429f87b1564c3514be5f08282bb80ae31
Paul Landes <landes@mailc.net>
2020-05-10 13:37:01 -0500

    cleanup

--- test/python/iris_model.py
+++ test/python/iris_model.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass, field
 from typing import Any, List
 from torch import nn
 from zensols.deeplearn import (
-    EarlyBailException,
     NetworkSettings,
     DeepLinearLayer,
     BaseNetworkModule,
