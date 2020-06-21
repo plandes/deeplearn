6faeefc42dc416dfddb009d067bb75df6cbaead0
Paul Landes <landes@mailc.net>
2020-06-21 18:38:11 -0500

    add override config merge to cli

--- src/python/zensols/deeplearn/cli/cli.py
+++ src/python/zensols/deeplearn/cli/cli.py
@@ -4,15 +4,17 @@
 __author__ = 'plandes'
 
 from typing import Type, Dict, Tuple, List
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 import logging
 import sys
 from io import TextIOWrapper
 from zensols.persist import dealloc
-from zensols.config import Configurable, Writable
+from zensols.config import Configurable, Writable, StringConfig
 from zensols.cli import OneConfPerActionOptionsCliEnv
 from zensols.deeplearn import TorchConfig
 from zensols.deeplearn.model import ModelFacade
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -40,7 +42,8 @@ class EnvironmentFormatter(Writable):
                 for k, v in ops.items():
                     self._write_line(f'{k.upper()}={v}', depth, writer)
             else:
-                cval = self.config.get_option_object(var.config_name, section=var.section)
+                cval = self.config.get_option_object(
+                    var.config_name, section=var.section)
                 line = f'{var.name.upper()}={str(cval)}'
                 self._write_line(line, depth, writer)
 
@@ -48,7 +51,15 @@ class EnvironmentFormatter(Writable):
 @dataclass
 class FacadeCli(object):
     config: Configurable
-    clear: bool = False
+    overrides: InitVar[str] = field(default=None)
+
+    def __post_init__(self, overrides: str):
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f'overrides: {overrides} ({type(overrides)}), ' +
+                         f'config: {self.config}')
+        if overrides is not None:
+            sc = StringConfig(overrides)
+            self.config.merge(sc)
 
     def _get_environment_formatter(self) -> EnvironmentFormatter:
         return EnvironmentFormatter()
@@ -91,6 +102,10 @@ class FacadeCommandLine(OneConfPerActionOptionsCliEnv):
         self.pkg_dist = kwargs['pkg_dist']
 
     def _get_actions(self) -> List[Dict[str, str]]:
+        overrides_op = ['-o', '--overrides', False,
+                        {'dest': 'overrides',
+                         'metavar': 'STRING',#'section.name=value[,section.name=value,...]',
+                         'help': 'comma separated config overrides'}]
         return [{'name': 'env',
                  'meth': 'print_environment',
                  'doc': 'action help explains how to do it'},
@@ -101,7 +116,7 @@ class FacadeCommandLine(OneConfPerActionOptionsCliEnv):
                 {'name': 'traintest',
                  'meth': 'train_test',
                  'doc': 'train and test the model',
-                 'opts': []}]
+                 'opts': [overrides_op]}]
 
     def _config_log_level(self, fmt, levelno):
         fmt = '%(asctime)s[%(levelname)s]:%(name)s %(message)s'
