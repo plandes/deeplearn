27c97254c539d4a342e074c35f53b13ad1340124
Paul Landes <landes@mailc.net>
2020-07-01 22:40:56 -0500

    doc

--- src/python/zensols/deeplearn/layer/linear.py
+++ src/python/zensols/deeplearn/layer/linear.py
@@ -28,7 +28,9 @@ class DeepLinearNetworkSettings(BasicNetworkSettings):
                             how much to grow or shrink as a percetage of the
                             last layer, otherwise, it's the number of features
 
-    :param proportions: how to treat the ``middle_features`` parameter
+    :param proportions: whether or not to interpret ``middle_features`` as
+                        a proportion of the previous layer or use directly
+                        as the size of the middle layer
 
     :param repeats: the number of repeats of the ``middle_features``
                     configuration
@@ -53,27 +55,6 @@ class DeepLinear(BaseNetworkModule, Deallocatable):
     def __init__(self, net_settings: DeepLinearNetworkSettings,
                  logger: logging.Logger = None):
         """Initialize the deep linear layer.
-
-        :param in_features: the number of features coming in to th network
-
-        :param out_features: the number of output features leaving the network
-
-        :param middle_features: a list of percent differences or exact
-                                parameter counts of each middle layer; if the
-                                former, the next shape is a function of the
-                                scaler multiplied by the previous layer; for
-                                example ``[1.0]`` creates a nested layer with
-                                the exact same shape as the input layer (see
-                                ``proportions`` parameter)
-
-        :param dropout: the droput used in all layers or ``None`` to disable
-
-        :param activation_function: the function between all layers, or
-                                    ``None`` for no activation
-
-        :param proportions: whether or not to interpret ``middle_features`` as
-                            a proportion of the previous layer or use directly
-                            as the size of the middle layer
 
         """
         super().__init__(net_settings, logger)
