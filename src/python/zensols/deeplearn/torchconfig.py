dda4094a70289d96c06b7b8d8bd987db74c472e6
Paul Landes <landes@mailc.net>
2020-08-20 13:35:33 -0500

    more torch type support

--- src/python/zensols/deeplearn/torchconfig.py
+++ src/python/zensols/deeplearn/torchconfig.py
@@ -3,7 +3,7 @@
 """
 __author__ = 'Paul Landes'
 
-from typing import List, Dict, Iterable, Any, Tuple, Union
+from typing import List, Dict, Iterable, Any, Tuple, Union, Type
 import sys
 import logging
 from io import TextIOBase
@@ -24,58 +24,69 @@ logger = logging.getLogger(__name__)
 
 class TorchTypes(object):
     TYPES = [{'desc': '32-bit floating point',
+              'name': 'float32',
               'types': set([torch.float32, torch.float]),
               'numpy': np.float32,
               'sparse': torch.sparse.FloatTensor,
               'cpu': torch.FloatTensor,
               'gpu': torch.cuda.FloatTensor},
              {'desc': '64-bit floating point',
+              'name': 'float64',
               'types': set([torch.float64, torch.double]),
               'numpy': np.float64,
               'sparse': torch.sparse.DoubleTensor,
               'cpu': torch.DoubleTensor,
               'gpu': torch.cuda.DoubleTensor},
              {'desc': '16-bit floating point',
+              'name': 'float16',
               'types': set([torch.float16, torch.half]),
               'numpy': np.float16,
               'sparse': torch.sparse.HalfTensor,
               'cpu': torch.HalfTensor,
               'gpu': torch.cuda.HalfTensor},
              {'desc': '8-bit integer (unsigned)',
+              'name': 'uint8',
               'types': set([torch.uint8]),
               'numpy': np.uint8,
               'sparse': torch.sparse.ByteTensor,
               'cpu': torch.ByteTensor,
               'gpu': torch.cuda.ByteTensor},
              {'desc': '8-bit integer (signed)',
+              'name': 'int8',
               'types': set([torch.int8]),
               'numpy': np.int8,
               'sparse': torch.sparse.CharTensor,
               'cpu': torch.CharTensor,
               'gpu': torch.cuda.CharTensor},
              {'desc': '16-bit integer (signed)',
+              'name': 'int16',
               'types': set([torch.int16, torch.short]),
               'numpy': np.int16,
               'sparse': torch.sparse.ShortTensor,
               'cpu': torch.ShortTensor,
               'gpu': torch.cuda.ShortTensor},
              {'desc': '32-bit integer (signed)',
+              'name': 'int32',
               'types': set([torch.int32, torch.int]),
               'numpy': np.int32,
               'sparse': torch.sparse.IntTensor,
               'cpu': torch.IntTensor,
               'gpu': torch.cuda.IntTensor},
              {'desc': '64-bit integer (signed)',
+              'name': 'int64',
               'types': set([torch.int64, torch.long]),
               'numpy': np.int64,
               'sparse': torch.sparse.LongTensor,
               'cpu': torch.LongTensor,
               'gpu': torch.cuda.LongTensor},
              {'desc': 'Boolean',
+              'name': 'bool',
               'types': set([torch.bool]),
               'numpy': np.bool,
               'cpu': torch.BoolTensor,
               'gpu': torch.cuda.BoolTensor}]
+
+    NAME_TO_TYPE = {t['name']: t for t in TYPES}
 
     FLOAT_TO_INT = {torch.float16: torch.int16,
                     torch.float32: torch.int32,
@@ -104,38 +115,43 @@ class TorchTypes(object):
         return self._types
 
     @classmethod
-    def get_tensor_class(self, torch_type: type, cpu_type: bool) -> type:
+    def type_from_string(self, type_name: str) -> torch.dtype:
+        types = self.NAME_TO_TYPE[type_name]['types']
+        return next(iter(types))
+
+    @classmethod
+    def get_tensor_class(self, torch_type: torch.dtype, cpu_type: bool) -> Type:
         types = self.types()
         entry = types[torch_type]
         key = 'cpu' if cpu_type else 'gpu'
         return entry[key]
 
     @classmethod
-    def get_sparse_class(self, torch_type: type) -> type:
+    def get_sparse_class(self, torch_type: torch.dtype) -> Type:
         types = self.types()
         entry = types[torch_type]
         return entry['sparse']
 
     @classmethod
-    def get_numpy_type(self, torch_type: type) -> type:
+    def get_numpy_type(self, torch_type: torch.dtype) -> Type:
         types = self.types()
         entry = types[torch_type]
         return entry['numpy']
 
     @classmethod
-    def float_to_int(self, torch_type: type) -> type:
+    def float_to_int(self, torch_type: torch.dtype) -> Type:
         return self.FLOAT_TO_INT[torch_type]
 
     @classmethod
-    def int_to_float(self, torch_type: type) -> type:
+    def int_to_float(self, torch_type: torch.dtype) -> Type:
         return self.INT_TO_FLOAT[torch_type]
 
     @classmethod
-    def is_float(self, torch_type: type) -> bool:
+    def is_float(self, torch_type: torch.dtype) -> bool:
         return torch_type in self.FLOAT_TYPES
 
     @classmethod
-    def is_int(self, torch_type: type) -> bool:
+    def is_int(self, torch_type: Type) -> bool:
         return torch_type in self.INT_TYPES
 
 
@@ -450,6 +466,36 @@ class TorchConfig(PersistableContainer, Writable):
             arr = arr.type(self.data_type)
         return arr
 
+    @property
+    def float_type(self) -> Type:
+        """Return the float type that represents this configuration, converting to the
+        corresponding precision from integer if necessary.
+
+        :return: the float that represents this data, or ``None`` if neither
+                 float nor int
+
+        """
+        dtype = self.data_type
+        if TorchTypes.is_int(dtype):
+            return TorchTypes.int_to_float(dtype)
+        elif TorchTypes.is_float(dtype):
+            return dtype
+
+    @property
+    def int_type(self) -> Type:
+        """Return the int type that represents this configuration, converting to the
+        corresponding precision from integer if necessary.
+
+        :return: the int that represents this data, or ``None`` if neither
+                 int nor float
+
+        """
+        dtype = self.data_type
+        if TorchTypes.is_float(dtype):
+            return TorchTypes.float_to_int(dtype)
+        elif TorchTypes.is_int(dtype):
+            return dtype
+
     @staticmethod
     def equal(a: torch.Tensor, b: torch.Tensor) -> bool:
         """Return whether or not two tensors are equal.  This does an exact cell
@@ -465,9 +511,9 @@ class TorchConfig(PersistableContainer, Writable):
 
         """
         return torch.allclose(a, b)
-
+    
     @classmethod
-    def get_random_seed(cls: type) -> int:
+    def get_random_seed(cls: Type) -> int:
         """Get the cross system random seed, meaning the seed applied to CUDA and the
         Python *random* library.
 
@@ -476,7 +522,7 @@ class TorchConfig(PersistableContainer, Writable):
             return cls.RANDOM_SEED['seed']
 
     @classmethod
-    def get_random_seed_context(cls: type) -> Dict[str, Any]:
+    def get_random_seed_context(cls: Type) -> Dict[str, Any]:
         """Return the random seed context given to :py:meth:`set_random_seed` to
         restore across models for consistent results.
 
@@ -484,7 +530,7 @@ class TorchConfig(PersistableContainer, Writable):
         return cls.RANDOM_SEED
 
     @classmethod
-    def set_random_seed(cls: type, seed: int = 0, disable_cudnn: bool = True,
+    def set_random_seed(cls: Type, seed: int = 0, disable_cudnn: bool = True,
                         rng_state: bool = True):
         """Set the random number generator for PyTorch.
 
