6c761b9b690e69b316fa95b41c27028193b4981c
Paul Landes <landes@mailc.net>
2020-08-22 08:33:39 -0500

    split split key container in to abstract and concrete impls

--- src/python/zensols/dataset/split.py
+++ src/python/zensols/dataset/split.py
@@ -6,6 +6,7 @@ __author__ = 'Paul Landes'
 
 from typing import Dict, Tuple
 from dataclasses import dataclass, field
+from abc import abstractmethod, ABCMeta
 import sys
 import logging
 from io import TextIOBase
@@ -20,24 +21,84 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass
-class StashSplitKeyContainer(SplitKeyContainer, Primeable, Writable):
+class AbstractSplitKeyContainer(SplitKeyContainer, Primeable,
+                                Writable, metaclass=ABCMeta):
+    key_path: Path
+    pattern: str
+    #pattern: str = field(default='{name}.dat')
+
+    def prime(self):
+        self._get_keys_by_split()
+
+    @abstractmethod
+    def _create_splits(self) -> Dict[str, Tuple[str]]:
+        pass
+
+    def _create_splits_and_write(self):
+        self.key_path.mkdir(parents=True, exist_ok=True)
+        for name, keys in self._create_splits().items():
+            fname = self.pattern.format(**{'name': name})
+            key_path = self.key_path / fname
+            with open(key_path, 'w') as f:
+                for k in keys:
+                    f.write(k + '\n')
+
+    def _read_splits(self):
+        by_name = {}
+        for path in self.key_path.iterdir():
+            p = parse.parse(self.pattern, path.name)
+            if p is not None:
+                p = p.named
+                if 'name' in p:
+                    with open(path) as f:
+                        by_name[p['name']] = tuple(
+                            map(lambda ln: ln.strip(), f.readlines()))
+        return by_name
+
+    @persisted('_split_names_pw')
+    def _get_split_names(self) -> Tuple[str]:
+        return frozenset(self.split_distribution.keys())
+
+    @persisted('_get_keys_by_split_pw')
+    def _get_keys_by_split(self) -> Dict[str, Tuple[str]]:
+        if not self.key_path.exists():
+            if logger.isEnabledFor(logging.INFO):
+                logger.info(f'creating key splits in {self.key_path}')
+            self._create_splits_and_write()
+        return self._read_splits()
+
+    def clear(self):
+        logger.debug('clearing split stash')
+        self.key_stash.clear()
+
+    def write(self, depth: int = 0, writer: TextIOBase = sys.stdout):
+        by_name = self.counts_by_key
+        total = sum(by_name.values())
+        self._write_line('key splits:', depth, writer)
+        for name, cnt in by_name.items():
+            self._write_line(f'{name}: {cnt} ({cnt/total*100:.1f}%)',
+                             depth + 1, writer)
+        self._write_line(f'total: {total}', depth, writer)
+
+
+@dataclass
+class StashSplitKeyContainer(AbstractSplitKeyContainer):
     stash: Stash
-    distribution: Dict[str, float]
-    path: Path
+    distribution: Dict[str, float] = field(default=None)
     shuffle: bool = field(default=True)
-    pattern: str = field(default='{name}.dat')
 
     def __post_init__(self):
+        super().__post_init__()
         sm = float(sum(self.distribution.values()))
         err, errm = (1. - sm), 1e-1
         if err > errm:
             raise ValueError('distriubtion must add to 1: ' +
                              f'{self.distribution} (err={err} > errm)')
 
-    def prime(self):
-        self._get_keys_by_split()
-
     def _create_splits(self) -> Dict[str, Tuple[str]]:
+        if self.distribution is None:
+            raise ValueError('must either provide `distribution` or ' +
+                             'implement `_create_splits`')
         by_name = {}
         keys = list(self.stash.keys())
         if self.shuffle:
@@ -59,49 +120,3 @@ class StashSplitKeyContainer(SplitKeyContainer, Primeable, Writable):
             print(k, len(v), len(v)/klen)
         assert sum(map(len, by_name.values())) == klen
         return by_name
-
-    def _create_splits_and_write(self):
-        self.path.mkdir(parents=True, exist_ok=True)
-        for name, keys in self._create_splits().items():
-            fname = self.pattern.format(**{'name': name})
-            path = self.path / fname
-            with open(path, 'w') as f:
-                for k in keys:
-                    f.write(k + '\n')
-
-    def _read_splits(self):
-        by_name = {}
-        for path in self.path.iterdir():
-            p = parse.parse(self.pattern, path.name)
-            if p is not None:
-                p = p.named
-                if 'name' in p:
-                    with open(path) as f:
-                        by_name[p['name']] = tuple(
-                            map(lambda ln: ln.strip(), f.readlines()))
-        return by_name
-
-    @persisted('_split_names_pw')
-    def _get_split_names(self) -> Tuple[str]:
-        return frozenset(self.split_distribution.keys())
-
-    @persisted('_get_keys_by_split_pw')
-    def _get_keys_by_split(self) -> Dict[str, Tuple[str]]:
-        if not self.path.exists():
-            if logger.isEnabledFor(logging.INFO):
-                logger.info(f'creating key splits in {self.path}')
-            self._create_splits_and_write()
-        return self._read_splits()
-
-    def clear(self):
-        logger.debug('clearing split stash')
-        self.key_stash.clear()
-
-    def write(self, depth: int = 0, writer: TextIOBase = sys.stdout):
-        by_name = self.counts_by_key
-        total = sum(by_name.values())
-        self._write_line('key splits:', depth, writer)
-        for name, cnt in by_name.items():
-            self._write_line(f'{name}: {cnt} ({cnt/total*100:.1f}%)',
-                             depth + 1, writer)
-        self._write_line(f'total: {total}', depth, writer)
