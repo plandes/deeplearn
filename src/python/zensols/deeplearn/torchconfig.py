f64dc748af48b2edf090028a229dad744172b225
Paul Landes <landes@mailc.net>
2021-02-25 18:18:09 -0600

    clone tensor correctly in 1.7 per warning

--- src/python/zensols/deeplearn/torchconfig.py
+++ src/python/zensols/deeplearn/torchconfig.py
@@ -253,6 +253,12 @@ class TorchConfig(PersistableContainer, Writable):
             tensor_or_model.type(self.data_type)
         return tensor_or_model
 
+    def clone(self, tensor: Tensor) -> Tensor:
+        """Clone a tensor.
+
+        """
+        return tensor.clone().detach().requires_grad_(True)
+
     def _populate_defaults(self, kwargs):
         """Add keyword arguments to typical torch tensor creation functions.
 
