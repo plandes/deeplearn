bd4ce30b1af20a320c7dd5353e2872f24ce3c0b3
Paul Landes <landes@mailc.net>
2021-04-02 11:29:29 -0500

    add test/train CLI app

--- src/python/zensols/deeplearn/cli/app.py
+++ src/python/zensols/deeplearn/cli/app.py
@@ -4,6 +4,7 @@ CLI.
 """
 __author__ = 'plandes'
 
+from typing import Dict, Any
 from dataclasses import dataclass, field
 import logging
 from zensols.persist import dealloc
@@ -21,13 +22,18 @@ class FacadeApplication(object):
     facade_name: str = field(default='facade')
     """The client facade."""
 
+    config_factory_args: Dict[str, Any] = field(default_factory=dict)
+    """The arguments given to the :class:`~zensols.config.ImportConfigFactory`,
+    which could be useful for reloading all classes while debugingg.
+
+    """
     def _create_facade(self) -> ModelFacade:
         """Create a new instance of the facade.
 
         """
         # we must create a new (non-shared) instance of the facade since it
         # will get deallcated after complete.
-        cf = ImportConfigFactory(self.config)
+        cf = ImportConfigFactory(self.config, **self.config_factory_args)
         facade = cf.instance(self.facade_name)
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug(f'created facade: {facade}')
@@ -64,7 +70,9 @@ class FacadeModelApplication(FacadeApplication):
                                       'short_name': 'p'}},
                 'mnemonic_overrides':
                 {'clear_batches': {'option_includes': set(),
-                                   'name': 'rmbatches'}}}
+                                   'name': 'rmbatch'},
+                 'batch': {'option_includes': set()},
+                 'train_production': 'trainprod'}}
 
     use_progress_bar: bool = field(default=False)
     """Display the progress bar."""
@@ -82,6 +90,13 @@ class FacadeModelApplication(FacadeApplication):
             logger.info('clearing batches')
             facade.batch_stash.clear()
 
+    def batch(self):
+        """Create batches (if not created already) and print statistics on the dataset.
+
+        """
+        with dealloc(self._create_facade()) as facade:
+            facade.executor.dataset_stash.write()
+
     def train(self):
         """Train the model and dump the results, including a graph of the
         train/validation loss.
@@ -90,3 +105,38 @@ class FacadeModelApplication(FacadeApplication):
         with dealloc(self._create_facade()) as facade:
             facade.train()
             facade.persist_result()
+
+    def test(self):
+        """Test an existing model the model and dump the results of the test.
+
+        """
+        with dealloc(self._create_facade()) as facade:
+            facade.test()
+
+    def train_test(self):
+        """Train, test the model, then dump the results with a graph.
+
+        """
+        with dealloc(self._create_facade()) as facade:
+            facade.train()
+            facade.test()
+            facade.persist_result()
+
+    def train_production(self):
+        """Train, test the model on train and test datasets, then dump the results with
+        a graph.
+
+        """
+        with dealloc(self._create_facade()) as facade:
+            facade.train_production()
+            facade.test()
+            facade.persist_result()
+
+    def early_stop(self):
+        """Stops the execution of training the model.
+
+        Currently this is done by creating a file the executor monitors.
+
+        """
+        with dealloc(self._create_facade()) as facade:
+            facade.executor.lifecycle_manager.stop()
