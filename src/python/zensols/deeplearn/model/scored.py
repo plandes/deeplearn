2c00560b1a677651e34e2e2946d8df4d11ed92b3
Paul Landes <landes@mailc.net>
2021-06-01 10:34:51 -0500

    doc; cleanup

--- src/python/zensols/deeplearn/model/scored.py
+++ src/python/zensols/deeplearn/model/scored.py
@@ -14,41 +14,64 @@ from . import BaseNetworkModule
 
 @dataclass
 class ScoredNetworkContext(object):
-    split_type: DatasetSplitType
+    """The forward context for the :class:`.ScoredNetworkModule`.  This is used in
+    :meth:`.ScoredNetworkModule._forward` to provide the module additional
+    information needed to score the model and produce the loss.
+
+    """
+
+    split_type: DatasetSplitType = field()
+    """The split type, which informs the module when decoding to produce outputs or
+    using the forward pass to prod.
+
+    :see: :meth:`.ScoredNetworkModule._forward`
+
+    """
     criterion: nn.Module = field()
+    """The criterion used to create the loss.  This is provided for modules that
+    produce the loss in the forward phase with the
+    `:meth:`torch.nn.module.forward`` method.
+
+    """
 
 
 @dataclass
 class ScoredNetworkOutput(object):
+    """The output from :clas:`.ScoredNetworkModule` modules.
+
+    """
+
     predictions: Tensor = field(repr=False)
     loss: Tensor
-    score: Tensor
+    score: Tensor = field(default=None)
 
 
 class ScoredNetworkModule(BaseNetworkModule):
-    """A module that has a forward training pass and a separate **scoring** phase.
+    """A module that has a forward training pass and a separate *scoring* phase.
     Examples include layers with an ending linear CRF layer, such as a BiLSTM
     CRF.  This module has a ``decode`` method that returns a 2D list of integer
     label indexes of a nominal class.
 
+    The context provides additional information needed to train, test and use
+    the module.
+
     :see: :class:`zensols.deeplearn.layer.RecurrentCRFNetwork`
 
+    .. document private functions
+    .. automethod:: _forward
+
     """
+
     @abstractmethod
     def _forward(self, batch: Batch, context: ScoredNetworkContext) -> \
             ScoredNetworkOutput:
+        """The forward pass, which either trains the model and creates the loss and/or
+        decodes the output for testing and evaluation.
+
+        :param batch: the batch to train, validate or test on
+
+        :param context: contains the additional information needed for scoring
+                        and decoding the sequence
+
+        """
         pass
-
-    # @abstractmethod
-    # def _score(self, batch: Batch, context: ScoredNetworkContext) -> Tuple[Tensor, Tensor]:
-    #     pass
-
-    # def score(self, batch: Batch, context: ScoredNetworkContext) -> Tuple[Tensor, Tensor]:
-    #     return self._score(batch, context)
-
-    # def get_loss(self, batch: Batch, criterion) -> Tensor:
-    #     if self.training:
-    #         raise ModelError('Resolving a loss while training results ' +
-    #                          'in the model training on the valdiation set--' +
-    #                          'use model.eval() first')
-    #     return self.forward(batch, criterion)
