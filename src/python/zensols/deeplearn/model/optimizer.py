dc0a2416f6f41483cc1d6ac144985c39e30c0dca
Paul Landes <landes@mailc.net>
2021-08-01 08:36:02 -0500

    cleanup; doc

--- src/python/zensols/deeplearn/model/optimizer.py
+++ src/python/zensols/deeplearn/model/optimizer.py
@@ -7,6 +7,13 @@ from abc import ABC, abstractmethod
 
 
 class ModelResourceFactory(ABC):
+    """An abstract factory that creates either an optimizer or criteria by the
+    :class:`.ModelExecutor`.  This is more of a marker class for other sub
+    classes to be configured and created by an
+    :class:`~zensols.config.ImportConfigFactory`.
+
+    """
     @abstractmethod
     def __call__(self, *args, **kwargs):
+        """Create the resource."""
         pass
