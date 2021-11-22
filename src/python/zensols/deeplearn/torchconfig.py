d06d8aa4edf53da30a674404972ac053a76aadd9
Paul Landes <landes@mailc.net>
2021-11-22 14:53:21 -0600

    remove pycuda dependency

--- src/python/zensols/deeplearn/torchconfig.py
+++ src/python/zensols/deeplearn/torchconfig.py
@@ -10,6 +10,7 @@ import gc
 from io import TextIOBase
 import random
 import torch
+import torch.cuda as cuda
 from torch import Tensor
 from torch import nn
 from torch.multiprocessing import set_start_method
@@ -26,31 +27,32 @@ class CudaInfo(Writable):
     the current (hardware) environment.
 
     """
+    @property
+    @persisted('_gpu_available', cache_global=True)
+    def gpu_available(self) -> bool:
+        return cuda.is_available()
 
     @property
+    @persisted('_num_devices', cache_global=True)
     def num_devices(self) -> int:
         """Return number of devices connected.
 
         """
-        import pycuda.driver as cuda
-        return cuda.Device.count()
+        return cuda.device_count()
 
-    @property
-    def attributes(self, device_id=0) -> dict:
-        """Get attributes of device with device Id = device_id
-
-        """
-        import pycuda.driver as cuda
-        return cuda.Device(device_id).get_attributes()
-
-    def write_attributes(self, writer=sys.stdout):
-        """Write the GPU attributes.
-
-        :see: attributes
-
-        """
-        for k, v in self.attributes.items():
-            writer.write(f'{k} -> {v}\n')
+    def get_devices(self, format: bool = False) -> Dict[int, Dict[str, Any]]:
+        devs = {}
+        for i in range(self.num_devices):
+            memory = dict(
+                reserved=cuda.memory_reserved(i),
+                allocated=cuda.memory_allocated(i),
+                total=cuda.get_device_properties(0).total_memory,
+            )
+            if format:
+                for k, v in memory.items():
+                    memory[k] = f'{memory[k]/1e9:.2f} GB'
+            devs[i] = dict(name=cuda.get_device_name(i), memory=memory)
+        return devs
 
     def write(self, depth: int = 0, writer: TextIOBase = sys.stdout):
         """Class representation as number of devices connected and about them.
@@ -58,14 +60,11 @@ class CudaInfo(Writable):
         :see: cuda
 
         """
-        import pycuda.driver as cuda
         num = self.num_devices
-        self._write_line(f'{num} device(s) found:', depth, writer)
-        for i in range(num):
-            name = f'{i+1}) {cuda.Device(i).name()} (Id: {i})'
-            mem = f'memory: {cuda.Device(i).total_memory()/1e9:.2f} GB'
-            self._write_line(name, depth + 1, writer)
-            self._write_line(mem, depth + 2, writer)
+        self._write_line(f'GPU available: {self.gpu_available}', depth, writer)
+        if self.gpu_available:
+            self._write_line(f'devices: ({num})', depth, writer)
+            self._write_object(self.get_devices(True), depth + 1, writer)
 
     def __str__(self):
         return f'CUDA devices: {self.num_devices}'
@@ -110,15 +109,13 @@ class TorchConfig(PersistableContainer, Writable):
         """Attempt to initialize CUDA, and if successful, return the CUDA device.
 
         """
+        # calling 
         is_avail = torch.cuda.is_available()
         use_gpu = self.use_gpu and is_avail
         logger.debug(f'use cuda: {self.use_gpu}, is avail: {is_avail}')
         if use_gpu:
-            import pycuda.driver as cuda
-            import pycuda.autoinit
-            logger.debug('trying to initialize CUDA')
-            cuda.init()
-            logger.info('successfully initialized CUDA')
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.info('successfully initialized CUDA')
             cuda_dev = torch.cuda.current_device()
             device = torch.device('cuda', cuda_dev)
             self.cuda_device_index = cuda_dev
