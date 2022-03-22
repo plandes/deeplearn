18f40a62624c6bc71c8e43041049c192f7482cd2
Paul Landes <landes@mailc.net>
2022-03-22 15:51:54 -0500

    add app CLI interface to prediction metrics

--- src/python/zensols/deeplearn/cli/app.py
+++ src/python/zensols/deeplearn/cli/app.py
@@ -200,6 +200,24 @@ class FacadeInfoApplication(FacadeApplication):
             reporter = ModelResultReporter(rm)
             reporter.dump(out_file)
 
+    def metrics(self, sort: str = 'wF1',
+                out_file: Path = None):
+        """Performance metrics across all archived results.
+
+        :param sort_col: the column to sort results
+
+        :param out_file: the output path
+
+        """
+        if out_file is None:
+            out_file = Path('metrics.csv')
+        with dealloc(self.create_facade()) as facade:
+            df = facade.get_predictions_factory().metrics_dataframe
+            df = df.sort_values(sort, ascending=False).reset_index(drop=True)
+            df.to_csv(out_file)
+            self._enable_cli_logging(facade)
+            logger.info(f'wrote: {out_file}')
+
     def result_ids(self):
         """Show all archived result IDs."""
         with dealloc(self.create_facade()) as facade:
